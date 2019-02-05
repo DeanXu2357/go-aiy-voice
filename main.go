0d7042bd2703b307f46265fc1cedc8316065d4f0
py <jasugun0000@gmail.com>
2019-02-05 08:24:26 +0800

    test gpio

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func main() {
+	fmt.Println("Test Start")
+
+	shinePin25()
+}
+
+func shinePin25() {
+	pin = rpio.Pin(10)
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmap gpio memory when done
+	defer rpio.Close()
+
+	// Set pin to output mode
+	pin.Output()
+
+	// Toggle pin 20 times
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
+}
