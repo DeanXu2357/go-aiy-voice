0e7689fb808960fcb04e3defb99dfebc45909b53
Dean Xu <dean.xu.2357@gmail.com>
2019-02-22 10:52:10 +0800

    tmp

--- aiy/voice/recorder.go
+++ aiy/voice/recorder.go
@@ -1,70 +1,84 @@
 package voice
 
 import (
-	"bytes"
 	"fmt"
 	"os/exec"
 	"time"
 )
 
-type VoiceForm struct{}
-
+// Recorder , the record
 type Recorder struct {
 	startEvent func()
-	endEvent   func()
+	stopEvent  func()
 	doneCh     chan bool
+	options    []string
 }
 
-func NewRecorder(starter func(), ender func()) (recorder Recorder, err error) {
-	// cmdString := produceCmdString()
+// NewRecorder , create a new Recorder struct
+func NewRecorder(starter func(), ender func(), afmt AudioFormat) (recorder Recorder, err error) {
+	options := produceCmdOptions(afmt)
 
-	go func() {
-		cmd := exec.Command("aplay", "-D", "", "")
-		var out bytes.Buffer
-		var stderr bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Stderr = &stderr
+	endSignal := make(chan bool)
 
-		if err = cmd.Start(); err != nil {
-			// todo log
-			return
-		}
-
-		done := make(chan error, 1)
-		go func() {
-			done <- cmd.Wait()
-		}()
-
-		select {
-		case <-time.After(3 * time.Second):
-			if err := cmd.Process.Kill(); err != nil {
-				fmt.Printf("failed to kill process %v\n", err)
-			}
-			fmt.Println("processkilled as timeout reached")
-		case err := <-done:
-			if err != nil {
-				fmt.Printf("process finished with error = %v", err)
-			}
-			fmt.Println("process finished successfully")
-		}
-	}()
-
-	// fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-	// fmt.Println("Result: " + out.String())
+	recorder = Recorder{startEvent: starter, stopEvent: ender, doneCh: endSignal, options: options}
 
 	return
 }
 
-func produceCmdString() string {
-	return ""
+func recordRun(options []string, endSignal chan bool) {
+	cmd := exec.Command("arecord", options...)
+	if err := cmd.Start(); err != nil {
+		// todo log
+		return
+	}
+
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case <-time.After(3 * time.Second):
+		if err := cmd.Process.Kill(); err != nil {
+			fmt.Printf("failed to kill process %v\n", err)
+		}
+		fmt.Println("processkilled as timeout reached")
+	case err := <-done:
+		if err != nil {
+			fmt.Printf("process finished with error = %v", err)
+		}
+		fmt.Println("process finished successfully")
+	case end := <-endSignal:
+		if end {
+			if err := cmd.Process.Kill(); err != nil {
+				fmt.Printf("failed to kill process %v\n", err)
+			}
+			fmt.Println("processkilled by end trigger")
+			close(endSignal)
+		}
+	}
 }
 
-func (recorder *Recorder) Start() {}
+func produceCmdOptions(afmt AudioFormat) (cmdString []string) {
+	return
+}
 
-func (recorder *Recorder) End() {}
+// Start , start recording
+func (recorder *Recorder) Start() {
+	go recordRun(recorder.options, recorder.doneCh)
+}
 
-func (recorder *Recorder) IsExist() {}
+// Stop , stop recording
+func (recorder *Recorder) Stop() {
+	recorder.doneCh <- true
+}
 
-func (recorder *Recorder) SetStartEvent() {}
+// SetStartEvent , set the recorder function triggered at start
+func (recorder *Recorder) SetStartEvent(fc func()) {
+	recorder.startEvent = fc
+}
 
-func (recorder *Recorder) SetEndEvent() {}
+// SetStopEvent , set the recorder function triggered at stop
+func (recorder *Recorder) SetStopEvent(fc func()) {
+	recorder.stopEvent = fc
+}
